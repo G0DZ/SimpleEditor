a0c1292101847679baf42222e73deb2e5846adae
rlurye <rm.lurye@gmail.com>
2017-10-18 02:38:58 +0300

    logo + splash screen

---
+++ app/src/main/java/lurie/rm/simpleeditor/App.kt
@@ -0,0 +1,15 @@
+package lurie.rm.simpleeditor
+
+import android.app.Application
+import android.os.SystemClock
+import java.util.concurrent.TimeUnit
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Don't do this! This is just so cold launches take some time
+        SystemClock.sleep(TimeUnit.SECONDS.toMillis(3))
+    }
+}
